b2cbf4aa0d365a35400d496189ee7b64759c386f
q1832565956 <1832565956@qq.com>
2018-05-23 16:43:39 +0800

    Initial commit

---
+++ webtest/src/main/java/com/tst/Test.java
@@ -0,0 +1,19 @@
+package com.tst;
+
+public class Test {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Test(String name) {
+        this.name = name;
+    }
+    
+}
